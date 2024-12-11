6cd377efafb15b2847406dfeaefc3a7df266d70e
Slava Haikin <vyachowski@gmail.com>
2024-12-11 15:58:20 +0700

    feat: add configuration checking and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: `${process.env.MONGODB_URL}${process.env.MONGODB_NAME}`,
+      authSource: 'admin',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+bootstrap();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
+  try {
+    checkConfiguration(configService);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+
+  configureSwagger(app);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+// function initializeDatabase(mongoDBUri: string) {
+// Получить данные
+// Проверить подключение
+// const typeOrmConnection = TypeOrmModule.
+// Проверить наличие базы
+// Если нет создать базу, если есть продолжить
+// }
+
+function configureSwagger(app: INestApplication<any>) {
   const config = new DocumentBuilder()
     .setTitle('Clean diet app API docs')
     .setDescription('Clean diet API description')
@@ -14,8 +40,18 @@ async function bootstrap() {
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-
-  await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap();
+function checkConfiguration(configService) {
+  const config = {
+    dbUrl: configService.get('MONGODB_URL'),
+    dbName: configService.get('MONGODB_NAME'),
+    envType: configService.get('ENV'),
+  };
+
+  Object.entries(config).forEach(([key, value]) => {
+    if (!value) {
+      throw new Error(`Configuration ${key} is not defined or empty.`);
+    }
+  });
+}
