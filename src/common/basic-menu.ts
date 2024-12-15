38a606f6d89909b9dc77990e7e347f7e2a2cf136
Slava Haikin <vyachowski@gmail.com>
2024-12-15 12:34:29 +0700

    feat: add all services to work with menu

--- src/common/basic-menu.ts
+++ src/common/basic-menu.ts
@@ -1,64 +1,66 @@
 import { Meal, Recipe } from './types';
 
-const basicMenu: Record<Meal, Recipe> = {
-  [Meal.Breakfast]: {
-    name: 'Scrambled Eggs with Avocado',
-    ingredients: [
-      { name: 'egg', amountInGramms: 200 },
-      { name: 'avocado', amountInGramms: 100 },
-      { name: 'spinach', amountInGramms: 50 },
-    ],
-    meal: Meal.Breakfast,
-    instruction:
-      'Whisk eggs and cook in a non-stick pan. Slice avocado and serve with sautéed spinach.',
-    calories: 452,
-  },
-  [Meal.Brunch]: {
-    name: 'Cottage Cheese with Walnuts',
-    ingredients: [
-      { name: 'cottage cheese', amountInGramms: 200 }, // ~180 kcal
-      { name: 'walnuts', amountInGramms: 20 }, // ~130 kcal
-    ],
-    meal: Meal.Brunch,
-    instruction: 'Serve cottage cheese topped with chopped walnuts.',
-    calories: 310,
-  },
-  [Meal.Lunch]: {
-    name: 'Grilled Chicken with Quinoa and Vegetables',
-    ingredients: [
-      { name: 'chicken breast', amountInGramms: 150 }, // ~165 kcal
-      { name: 'quinoa', amountInGramms: 120 }, // ~220 kcal
-      { name: 'broccoli', amountInGramms: 100 }, // ~34 kcal
-      { name: 'carrots', amountInGramms: 80 }, // ~33 kcal
-    ],
-    meal: Meal.Lunch,
-    instruction:
-      'Grill chicken breast. Cook quinoa according to package instructions. Steam broccoli and carrots.',
-    calories: 452,
-  },
-  [Meal.Snack]: {
-    name: 'Greek Yogurt with Berries and Nuts',
-    ingredients: [
-      { name: 'greek yogurt', amountInGramms: 150 }, // ~100 kcal
-      { name: 'blueberries', amountInGramms: 50 }, // ~28 kcal
-      { name: 'almonds', amountInGramms: 20 }, // ~120 kcal
-    ],
-    meal: Meal.Snack,
-    instruction:
-      'Mix Greek yogurt with fresh blueberries and top with chopped almonds.',
-    calories: 248,
-  },
-  [Meal.Dinner]: {
-    name: 'Salmon with Sweet Potato and Asparagus',
-    ingredients: [
-      { name: 'salmon', amountInGramms: 150 }, // ~250 kcal
-      { name: 'sweet potato', amountInGramms: 200 }, // ~180 kcal
-      { name: 'asparagus', amountInGramms: 100 }, // ~20 kcal
-    ],
-    meal: Meal.Dinner,
-    instruction:
-      'Bake salmon with olive oil and herbs. Roast sweet potato and asparagus.',
-    calories: 450,
+const basicMenu: { meals: Record<Meal, Recipe> } = {
+  meals: {
+    [Meal.Breakfast]: {
+      name: 'Scrambled Eggs with Avocado',
+      ingredients: [
+        { name: 'egg', amountInGramms: 200 },
+        { name: 'avocado', amountInGramms: 100 },
+        { name: 'spinach', amountInGramms: 50 },
+      ],
+      meal: Meal.Breakfast,
+      instruction:
+        'Whisk eggs and cook in a non-stick pan. Slice avocado and serve with sautéed spinach.',
+      calories: 452,
+    },
+    [Meal.Brunch]: {
+      name: 'Cottage Cheese with Walnuts',
+      ingredients: [
+        { name: 'cottage cheese', amountInGramms: 200 }, // ~180 kcal
+        { name: 'walnuts', amountInGramms: 20 }, // ~130 kcal
+      ],
+      meal: Meal.Brunch,
+      instruction: 'Serve cottage cheese topped with chopped walnuts.',
+      calories: 310,
+    },
+    [Meal.Lunch]: {
+      name: 'Grilled Chicken with Quinoa and Vegetables',
+      ingredients: [
+        { name: 'chicken breast', amountInGramms: 150 }, // ~165 kcal
+        { name: 'quinoa', amountInGramms: 120 }, // ~220 kcal
+        { name: 'broccoli', amountInGramms: 100 }, // ~34 kcal
+        { name: 'carrots', amountInGramms: 80 }, // ~33 kcal
+      ],
+      meal: Meal.Lunch,
+      instruction:
+        'Grill chicken breast. Cook quinoa according to package instructions. Steam broccoli and carrots.',
+      calories: 452,
+    },
+    [Meal.Snack]: {
+      name: 'Greek Yogurt with Berries and Nuts',
+      ingredients: [
+        { name: 'greek yogurt', amountInGramms: 150 }, // ~100 kcal
+        { name: 'blueberries', amountInGramms: 50 }, // ~28 kcal
+        { name: 'almonds', amountInGramms: 20 }, // ~120 kcal
+      ],
+      meal: Meal.Snack,
+      instruction:
+        'Mix Greek yogurt with fresh blueberries and top with chopped almonds.',
+      calories: 248,
+    },
+    [Meal.Dinner]: {
+      name: 'Salmon with Sweet Potato and Asparagus',
+      ingredients: [
+        { name: 'salmon', amountInGramms: 150 }, // ~250 kcal
+        { name: 'sweet potato', amountInGramms: 200 }, // ~180 kcal
+        { name: 'asparagus', amountInGramms: 100 }, // ~20 kcal
+      ],
+      meal: Meal.Dinner,
+      instruction:
+        'Bake salmon with olive oil and herbs. Roast sweet potato and asparagus.',
+      calories: 450,
+    },
   },
 };
 
--- src/menu/menu.service.ts
+++ src/menu/menu.service.ts
@@ -1,15 +1,15 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { DataSource, ObjectId } from 'typeorm';
 import { Menu } from './entities/menu.entity';
 import basicMenu from 'src/common/basic-menu';
+import { CreateMenuDto } from './dto/create-menu.dto';
+import { UpdateMenuDto } from './dto/update-menu.dto';
 
 @Injectable()
 export class MenuService implements OnModuleInit {
   constructor(private dataSource: DataSource) {}
 
   async onModuleInit() {
-    const initialMenu = { meals: basicMenu };
-
     try {
       const count = await this.dataSource.manager.count(Menu);
 
@@ -19,41 +19,62 @@ export class MenuService implements OnModuleInit {
         console.log(
           'Menu collection exists but is empty, data will be created.',
         );
-        const newMenu = await this.dataSource.mongoManager.create(
-          Menu,
-          initialMenu,
-        );
+
+        const newMenu = this.dataSource.mongoManager.create(Menu, basicMenu);
         const result = await this.dataSource.manager.save(newMenu);
+
         console.log('Data was created:', result);
       }
     } catch {
       console.error('Error checking menu collection, data will be created.');
-      const newMenu = await this.dataSource.mongoManager.create(
-        Menu,
-        initialMenu,
-      );
+
+      const newMenu = this.dataSource.mongoManager.create(Menu, basicMenu);
       const result = await this.dataSource.manager.save(newMenu);
+
       console.log('Data was created:', result);
     }
   }
 
-  // async create(createMenuDto: CreateMenuDto) {
-  //   return await this.menuRepository.create(createMenuDto);
-  // }
+  async create(createMenuDto: CreateMenuDto) {
+    const menu = this.dataSource.mongoManager.create(Menu, createMenuDto);
+    return await this.dataSource.mongoManager.save(menu);
+  }
 
-  // async findAll() {
-  //   return await this.menuRepository.find();
-  // }
+  async findAll() {
+    return await this.dataSource.mongoManager.find(Menu);
+  }
 
-  // findOne(id: number) {
-  //   return `This action returns a #${id} menu`;
-  // }
+  async findOne(id: number) {
+    return await this.dataSource.mongoManager.findOneBy(Menu, id);
+  }
 
-  // update(id: number, updateMenuDto: UpdateMenuDto) {
-  //   return `This action updates a #${id} menu`;
-  // }
+  async update(id: number, updateMenuDto: UpdateMenuDto) {
+    const editingMenu = await this.dataSource.mongoManager.findOneBy(Menu, id);
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} menu`;
-  // }
+    if (editingMenu) {
+      throw new Error(`Menu with id ${id} not found`);
+    }
+
+    const updatedMenu = {
+      ...editingMenu,
+      ...updateMenuDto,
+    };
+
+    const createdMenu = this.dataSource.mongoManager.create(Menu, updatedMenu);
+    return await this.dataSource.mongoManager.save(createdMenu);
+  }
+
+  async remove(id: number) {
+    const objectId = new ObjectId(id);
+
+    const result = await this.dataSource.mongoManager.deleteOne(Menu, {
+      _id: objectId,
+    });
+
+    if (result.deletedCount === 0) {
+      throw new Error(`Menu with id ${id} not found`);
+    }
+
+    return { message: `Menu with id ${id} has been removed` };
+  }
 }
